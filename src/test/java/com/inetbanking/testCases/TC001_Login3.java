b3f57d8665be44d12a0daece2ceffa4f7c35c02e
Lenovo <Lenovo@Lenovo-PC>
2020-06-25 13:49:43 +0530

    Updates test cases

---
+++ src/test/java/com/inetbanking/testCases/TC001_Login3.java
@@ -0,0 +1,41 @@
+package com.inetbanking.testCases;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import com.inetbanking.pageObjects.LoginPage;
+
+import junit.framework.Assert;
+
+public class TC001_Login3 extends BaseClass {
+	
+	
+	@Test
+	public void loginTest() throws IOException
+	{
+		
+		
+		logger.info("url is opened");
+		//driver.manage().window().maximize();
+		LoginPage lp = new LoginPage(driver);
+		lp.setUsername(userName);
+		logger.info("usrname is entered");
+		lp.setPassword(password);
+		logger.info("password is entered");
+		
+		lp.clickButton();
+		
+		System.out.println(driver.getTitle());		
+		if(driver.getTitle().equals("Guru99 Bank Manager HomePage"))
+			Assert.assertTrue(true);
+		else
+		{
+				
+		captureScreen(driver, "logintest");
+		Assert.assertTrue(false);
+		logger.info("logintest is failed");
+		}
+	}
+
+}
